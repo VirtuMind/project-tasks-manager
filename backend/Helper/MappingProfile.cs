f019528c0bda74d5a7db0760a329a0ce2c167f63
Younes Khoubaz <youness.dobarim@gmail.com>
2025-12-16 20:28:44 +0100

    Mapping profiles for automapper

---
+++ backend/Helper/MappingProfile.cs
@@ -0,0 +1,30 @@
+using AutoMapper;
+using ProjectTasksManager.DTOs;
+using ProjectTasksManager.Models;
+
+namespace ProjectTasksManager.Helper;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<User, UserDto>();
+
+        CreateMap<Project, ProjectDto>()
+            .ForMember(dest => dest.Stats, opt => opt.MapFrom(src => new ProjectProgressDto
+            {
+                TotalTasks = src.Tasks.Count,
+                CompletedTasks = src.Tasks.Count(t => t.IsCompleted),
+                ProgressPercentage = src.Tasks.Count > 0
+                    ? Math.Round((double)src.Tasks.Count(t => t.IsCompleted) / src.Tasks.Count * 100, 2)
+                    : 0
+            }));
+
+        CreateMap<CreateProjectRequest, Project>();
+        CreateMap<UpdateProjectRequest, Project>();
+
+        CreateMap<ProjectTask, TaskDto>();
+        CreateMap<CreateTaskRequest, ProjectTask>();
+        CreateMap<UpdateTaskRequest, ProjectTask>();
+    }
+}
